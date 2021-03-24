3934cdd368ead341a8cc9bc29360f793f690b6cd
Renato Siqueira <renatoelysiqueira@gmail.com>
2021-03-24 18:27:11 -0300

    Teste

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ const express = require('express')
 const app = express()
 const axios = require('axios')
 const bodyParser = require('body-parser')
+const path = require('path')
 
 const categorias = require('./routes/categorias')
 const publicacoes = require('./routes/publicacoes')
 
 app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, './views'))
 app.use(bodyParser.urlencoded())
 
 const port = process.env.PORT || 3000
